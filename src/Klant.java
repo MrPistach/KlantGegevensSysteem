f3f155f074fbc4839fb9dbe313f87b8bf2eefe98
Khalid <khalid040804@gmail.com>
2023-03-28 16:32:36 +0200

    test

---
+++ src/Klant.java
@@ -0,0 +1,29 @@
+public class Klant {
+    private String naam;
+    private String email;
+    private int telefoonnummer;
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(int telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+}
